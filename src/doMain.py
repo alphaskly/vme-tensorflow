8bb35d19215fa8b008dd88f753317c56c4a53589
yubb <2601169057@qq.com>
2017-08-17 17:32:28 +0800

    machine learning

--- src/doMain.py
+++ src/doMain.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-hello = tf.constant('Hello,TensorFlow!')
+hello = tf.constant('Hello,TensorFlow!I am Vme...')
 sess = tf.Session()
 print(sess.run(hello))
 a = tf.constant(10)
