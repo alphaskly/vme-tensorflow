0075f688a2c39e7d9065d50667ce977e02c2401b
yubb <2601169057@qq.com>
2017-08-19 21:27:45 +0800

    add doc

--- src/learning/tensor.py
+++ src/learning/tensor.py
@@ -1,30 +1,46 @@
+#coding: utf-8
 import tensorflow as tf
 
+class Tensor:
 
-def one():
-    a = tf.constant([1.0, 2.0], name="a")
-    b = tf.constant([2.0, 3.0], name='b')
-    result = a + b
-    print(a.graph() is tf.get_default_graph())
+    '''
+        TensorFlow Two stage:
+            1、定义计算图中的计算
+            2、执行计算
+        每个TensorFlow程序都有一个默认的计算图
+    '''
 
-def two():
-    print(tf.__version__)
-    g1 = tf.Graph()
-    with g1.as_default():
-        v = tf.get_variable("v", [1])
+    def one(self):
+        #定义计算
+        a = tf.constant([1.0, 2.0], name="a")
+        b = tf.constant([2.0, 3.0], name='b')
+        result = a + b
+        print(result)
 
-    g2 = tf.Graph()
-    with g2.as_default():
-        v = tf.get_variable("v", [1])
+    def two(self):
+        print(tf.__version__)
+        #生成计算图(不同计算图上的张量和运算不会共享)
+        g1 = tf.Graph()
+        with g1.as_default():
+            #在计算图中添加变量,并初始化
+            v = tf.get_variable("v", [1])
 
-    with tf.Session(graph=g1) as sess:
-        tf.initialize_all_variables().run()
-        with tf.variable_scope("", reuse=True):
-            print(sess.run(tf.get_variable("v")))
-    with tf.Session(graph=g2) as sess:
-        tf.initialize_all_variables().run()
-        with tf.variable_scope("", reuse=True):
-            print(sess.run(tf.get_variable("v")))
+        g2 = tf.Graph()
+        with g2.as_default():
+            v = tf.get_variable("v", [1])
+
+        #读取变量
+        with tf.Session(graph=g1) as sess:
+            tf.initialize_all_variables().run()
+            with tf.variable_scope("", reuse=True):
+                print(sess.run(tf.get_variable("v")))
+        with tf.Session(graph=g2) as sess:
+            tf.initialize_all_variables().run()
+            with tf.variable_scope("", reuse=True):
+                print(sess.run(tf.get_variable("v")))
 
 
-two()
+tensor = Tensor()
+tensor.one()
+tensor.two()
+print(Tensor.__doc__)
